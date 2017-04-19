b33e41d767bc0712eba0599dd47d02e004f244d5
KH <khendarg@ucsd.edu>
2017-04-19 15:52:25 -0700

    ~finished tc2pdb

--- tc2pdb.py
+++ tc2pdb.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 
-import subprocess, tempfile, os
+import subprocess, tempfile, os, re
 
 def pullfasta(query, tcfa):
 
@@ -34,12 +34,36 @@ def pdblast(fastas, ident=30, alnlen=60):
 		blastout = subprocess.check_output(['blastp', '-db', 'pdbaa', '-query', f.name, '-outfmt', '7'])
 	finally: os.remove(f.name)
 
+	results = []
 	for l in blastout.split('\n'):
 		if l.strip().startswith('#'): continue
 		if not l.strip(): continue
 		row = l.split('\t')
 		if float(row[2]) < ident or int(row[3]) < alnlen: continue
-		print(row[0:2])
+		results.append(row)
+	return results
+
+def select_best_matches(blasts, depth=5):
+	#{PDB: [(score, TCID), (score, TCID)]}
+
+	exp = '[0-9]\.[A-Z]\.'
+	if depth >= 3: exp += '[0-9]+\.'
+	if depth >= 4: exp += '[0-9]+\.'
+	if depth >= 5: exp += '[0-9]+'
+
+	table = {}
+	for l in blasts:
+		tcid = re.findall(exp, l[0])[0]
+		score = l[11]
+		#maybe use e-value instead?
+
+		try: table[l[1]].append((score, tcid))
+		except KeyError: table[l[1]] = [(score, tcid)]
+	finaltable = {}
+	for k in sorted(table.keys()):
+		#print(k, sorted(table[k])[::-1])
+		finaltable[k] = sorted(table[k])[::-1][0][1]
+	return finaltable
 
 if __name__ == '__main__':
 	import argparse
@@ -54,8 +78,12 @@ if __name__ == '__main__':
 	fastas = ''
 
 	tcids = []
+	blasts = []
 	for tcid in args.tcids:
 		
 		if tcid.count('.') == 4 and not tcid.endswith(' '): tcid += ' '
 		if tcid.count('.') < 4 and not tcid.endswith('.'): tcid += '.'
-		pdblast(pullfasta(tcid, args.i))
+		blasts += pdblast(pullfasta(tcid, args.i))
+	out = select_best_matches(blasts)
+	#use $'\t'
+	for pdb in sorted(out): print(pdb + '\t' + out[pdb])
