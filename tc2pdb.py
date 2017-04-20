2f19f1b6ab6cf526ef6b255676a998d31bcf24a3
KH <khendarg@ucsd.edu>
2017-04-19 20:10:38 -0700

    Mostly done with tc2pdb, probably

--- tc2pdb.py
+++ tc2pdb.py
@@ -6,6 +6,7 @@ import subprocess, tempfile, os, re, sys
 VERBOSITY = 0
 def pullfasta(query, tcfa):
 
+	if not query.startswith('|'): query = '|' + query
 	f = open(tcfa)
 	contents = f.read()
 	f.close()
@@ -15,6 +16,7 @@ def pullfasta(query, tcfa):
 	for l in contents.split('\n'):
 		if not recording:
 			if l.startswith('>') and query in l:
+				#if VERBOSITY: print('[INFO]:', l, file=sys.stderr)
 				recording = 1
 				out += l + '\n'
 			else: continue
@@ -27,7 +29,7 @@ def pullfasta(query, tcfa):
 
 	return out.strip()
 
-def pdblast(fastas, ident=30, alnlen=60, evalue=0.05):
+def pdblast(fastas, ident=10, alnlen=60, evalue=0.05):
 	f = tempfile.NamedTemporaryFile(delete=False)
 	f.write(fastas)
 	f.close()
@@ -54,9 +56,9 @@ def pdblast(fastas, ident=30, alnlen=60, evalue=0.05):
 			continue
 		results.append(row)
 	if VERBOSITY: 
-		print('[INFO]: %s hit(s) rejected due to at least insufficient identity (<%s)' % (ifail, ident), file=sys.stderr)
-		print('[INFO]: %s hit(s) rejected due to at least insufficient alignment length (<%s)' % (lfail, alnlen), file=sys.stderr)
-		print('[INFO]: %s hit(s) rejected due to excessive E-value (>%s)' % (efail, evalue), file=sys.stderr)
+		if ifail: print('[INFO]: %s hit(s) rejected due to insufficient identity (<%s) (and maybe other things)' % (ifail, ident), file=sys.stderr)
+		if lfail: print('[INFO]: %s hit(s) rejected due to insufficient alignment length (<%s) (and maybe other things)' % (lfail, alnlen), file=sys.stderr)
+		if efail: print('[INFO]: %s hit(s) rejected due to excessive E-value (>%s)' % (efail, evalue), file=sys.stderr)
 	return results
 
 def select_best_matches(blasts, depth=5):
@@ -137,14 +139,20 @@ if __name__ == '__main__':
 
 	tcids = []
 	blasts = []
+	done = 0
 	for tcid in args.tcids:
-		
 		if tcid.count('.') == 4 and not tcid.endswith(' '): tcid += ' '
 		if tcid.count('.') < 4 and not tcid.endswith('.'): tcid += '.'
+		fastas = pullfasta(tcid, args.f)
+		done += fastas.count('>')
+		if VERBOSITY: print('[INFO]: BLASTing %s sequences' % done, file=sys.stderr)
+
 		blasts += pdblast(pullfasta(tcid, args.f ))
 	out = select_best_matches(blasts)
 	#use $'\t'
 	#for pdb in sorted(out): print(pdb + '\t' + out[pdb])
+	#0.33, 0.30s/TCDB entry (at 5th level)
+	#sed 's/_.*$//g' ../all_tcpdb | sort | uniq | wc > getlist
 	if not args.i: filter_membranes(out)
 		
 	pretty_print_table(out)
